5fe93faf77ada7be58c9420e97bd941c0d1975fd
oliverio89 <oliverio89@gmail.com>
2023-03-09 15:38:46 +0100

    home page

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,12 +1,20 @@
 import React from 'react'
 import './HomePage.css'
+import {  Container, Button  } from 'react-bootstrap'
 
 
 const HomePage = () => {
+    
+
 
     return (
         <>
-        <div></div>
+        <div className='presentacion'>
+        <h1> Hellow world</h1>
+        <p> Hola mi nombre es Víctor,
+        soy un diseñador y desarrollador web, residiendo en Madrid, España.</p>
+           
+        </div>
         <div className="hero" />
         </>
     )
