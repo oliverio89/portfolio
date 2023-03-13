22d9c4650f64f688def0b06434771a10fd09ceac
oliverio89 <115175561+oliverio89@users.noreply.github.com>
2023-03-13 10:36:01 +0100

    first commit in day, finish hero

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,23 +1,34 @@
 import React from 'react'
 import './HomePage.css'
-import {  Container, Button  } from 'react-bootstrap'
 import myPhoto from '../../resources/images/IMG_9956.JPG'
 
 
 const HomePage = () => {
-    
+
 
 
     return (
         <>
-        <div className='presentacion'>
-        <h1> Hellow world</h1>
-        <p> Hola mi nombre es Víctor,
-        soy un diseñador y desarrollador web, residiendo en Madrid, España.</p>
-        <img src={myPhoto} className='imgMyPhoto'/>
-        </div>
-        
-        <div className="hero" />
+            <div className='presentacion'>
+                <div className="titulo">
+                    <h1> My name is Víctor</h1>
+
+
+                    <p> Hola mi nombre es Víctor,<br />
+                        soy un diseñador y desarrollador web, residiendo en Madrid, España.</p>
+                </div>
+                <div className='photo'>
+                    <img src={myPhoto} className='imgMyPhoto' />
+                </div>
+            </div>
+
+            <div className="hero" />
+
+            {/* cuerpo */}
+            <div className='cuerpo'>
+
+            </div>
+
         </>
     )
 }
