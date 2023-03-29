247d1481725cf3cffc53833da78d0154f3f44871
oliverio89 <oliverio89@gmail.com>
2023-03-29 13:33:20 +0200

    seguimos trabajando

--- src/components/Language/es.jsx
+++ src/components/Language/es.jsx
@@ -1,5 +1,5 @@
 const es = {
-    "titulo": "Mi sitio web en español",
+    "titulo": "Mi nombre es Víctor",
     "boton": "Cambiar idioma a inglés",
     "descripción": "Soy un desarrollador web junior con pasión por la creación de sitios web dinámicos y atractivos. Con un fuerte conocimiento en lenguajes de programación como HTML, CSS y JavaScript, me enorgullece crear soluciones eficaces para problemas complejos y ayudar a las empresas a tener presencia en línea. Además, tengo experiencia trabajando en equipo y colaborando en proyectos multidisciplinarios, lo que me ha permitido desarrollar habilidades de comunicación efectiva y trabajo en equipo. Estoy emocionado de seguir aprendiendo y creciendo como desarrollador, y espero poder aplicar mis habilidades y conocimientos para ayudar a las empresas a alcanzar sus objetivos en línea.",
     "AcercaDeMí": "Acerca de mí",
--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -5,6 +5,7 @@ import es from '../../components/Language/es';
 import en from '../../components/Language/en';
 import espana from '../../resources/images/icons8-spain-48.png';
 import inglaterra from '../../resources/images/icons8-united-kingdom-48.png';
+import Contact from '../../components/Contact/contact'
 
 
 
@@ -21,13 +22,7 @@ const HomePage = () => {
             <div className='presentacion'>
                 <div className="titulo">
                 <h1>{idioma === 'es' ? es.titulo : en.titulo}</h1>
-                    <p> Soy un desarrollador web junior con pasión por la creación de sitios web dinámicos y atractivos.<br></br> 
-                    Con un fuerte conocimiento en lenguajes de programación como HTML, CSS y JavaScript, <br></br>
-                    me enorgullece crear soluciones eficaces para problemas complejos y ayudar a las empresas a tener presencia en línea.<br></br> 
-                    Además, tengo experiencia trabajando en equipo y colaborando en proyectos multidisciplinarios, lo que me ha permitido desarrollar <br></br>
-                    habilidades de comunicación efectiva y trabajo en equipo.<br></br>
-                    Estoy emocionado de seguir aprendiendo y creciendo como desarrollador, y espero poder aplicar mis habilidades y conocimientos para ayudar <br></br>
-                    a las empresas a alcanzar sus objetivos en línea.</p>
+                    <p> {idioma === 'es' ? es.descripción : en.descripción}</p>
                 </div>
                 <div class="bg-image hover-zoom">
                     <img src={myPhoto} className='imgMyPhoto bg-image hover-zoom' />
@@ -46,6 +41,7 @@ const HomePage = () => {
             <div className='cuerpo'>
 
             </div>
+            <div> <Contact /> </div>
 
         </>
     )
