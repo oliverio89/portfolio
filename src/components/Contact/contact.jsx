2a22c06f0dd66983bdaeacbdfcf00ef753275e91
oliverio89 <oliverio89@gmail.com>
2023-04-04 13:18:01 +0200

    initial contacts

--- src/components/Contact/contact.jsx
+++ src/components/Contact/contact.jsx
@@ -1,8 +1,24 @@
+import React from 'react';
+import { FaLinkedin, FaGithub, FaEnvelope } from 'react-icons/fa';
+import './contact.css'
 
 function Contact() {
 
 
-    return <h1>hello World</h1>
+    return    <div className="contact-links">
+
+      <a href="https://www.linkedin.com/in/victor-wdfs" target="_blank" rel="noopener noreferrer">
+        <FaLinkedin className="contact-icon" />
+      </a>
+      <a href="https://github.com/oliverio89" target="_blank" rel="noopener noreferrer">
+        <FaGithub className="contact-icon" />
+      </a>
+      {/* <a href="mailto:oliverio89@gmail.com" target="_blank" rel="noopener noreferrer">
+        <FaEnvelope className="contact-icon" />
+      </a> */}
+  </div>
+
+   
 }
 
 export default Contact
