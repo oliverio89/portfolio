df420d664c2c96c51534414b996752826fd1e3f0
oliverio89 <oliverio89@gmail.com>
2023-05-04 17:22:43 +0200

    ultimo deploy para netlify

--- src/index.js
+++ src/index.js
@@ -4,13 +4,16 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter as Router } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <Router basename={process.env.PUBLIC_URL}>
-      <App />
-    </Router>
+    <HashRouter>
+      <Router basename={process.env.PUBLIC_URL}>
+        <App />
+      </Router>
+    </HashRouter>
   </React.StrictMode>
 );
 
